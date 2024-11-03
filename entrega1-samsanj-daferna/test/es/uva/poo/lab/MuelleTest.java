b77a5496f52cdce3bbea8809ca078351153825a1
Danii47 <uranux1@gmail.com>
2024-11-03 02:34:20 +0100

    añadido muelleTest y puertoTest

---
+++ entrega1-samsanj-daferna/test/es/uva/poo/lab/MuelleTest.java
@@ -0,0 +1,14 @@
+package es.uva.poo.lab;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MuelleTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ entrega1-samsanj-daferna/test/es/uva/poo/lab/PuertoTest.java
@@ -0,0 +1,14 @@
+package es.uva.poo.lab;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PuertoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
